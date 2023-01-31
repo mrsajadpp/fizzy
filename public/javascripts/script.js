72d2fecabbcc8208c0425ac54124c78fdc758b13
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2023-01-31 17:22:27 +0530

    Add files via upload

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,9 @@
+let mainBody = document.getElementById('main');
 function hideLoader() {
     setTimeout(() => {
         let loader = document.getElementById('loader');
         loader.style.display = 'none';
+        mainBody.classList.remove('hide');
     }, 3000);
 }
 
